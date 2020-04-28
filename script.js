d47e2d1bd9ca6040a938841a28c8d2e02fb6c4b0
lyndexer <mckinney.m93@gmail.com>
2020-04-28 15:53:10 -0700

    issues with getting player marker to print to grid

--- script.js
+++ script.js
@@ -1,66 +1,93 @@
 //##########################gameBoard obj (module)############################################//
-const gameBoard = (function () {
+const gameBoard = (() => {
     /*store the gameboard as an array inside of a Gameboard object*/
-    const square= ['','','','','','','','',''];
+    const square = ['', '', '', '', '', '', '', '', ''];
     return {
         square
     };
 })();
 
 
-
-
-
-
-
-
-
 //#########################displayController (module)###########################################//
-const displayController = (function () { 
-//determines if the spot marked should be an X or an O based on who's turn it is
-    function markBox(e) {
-        let boxes= document.querySelectorAll('.box')
+const displayController = (() => {
+    //determines if the spot marked should be an X or an O based on who's turn it is
+    const boxes = document.querySelectorAll('.box');
+    playerTurnText = document.getElementById('playerTurnText');
+    playerTurnText.textContent = 'Player 1 Turn';
+    const markGrid= () => {
         document.addEventListener('click', function printToGrid(event) {
             if(event.target.matches('.box')) {
-                console.log('hello!')
-            }else {
-                console.log('boo')
+                for(let i= 0; i < boxes.length; i++) {
+                    if(event.target.matches(`.box${i}`)) {
+                        let newMark;
+                    }
+                    if(playerTurn()=== 'p1') {
+                        newMark = 'X'
+                    }
+                    if(playerTurn()=== 'p2') {
+                        newmark = 'O'
+                    }
+                    return gameBoard.square.splice(i, 1, newMark)
+                }
             }
-        }
-        )};
+
+    }, false);
+}
 
 
-//prints X's and O's onto the screen
-    function render() {
-        const boxes= document.querySelectorAll('.box')
-    }
-    return {
-        markBox
+
+
+
+    //reset button
+
+    //prints X's and O's onto the screen by render function
+    /*Render() {
+
     };
-})();
+    */
 
-
-
-
-
-
-
-
-
-
-
+    return {
+        markGrid
+    };
+    
+});
 
 
 //###############################players-2 (Factory)##########################################//
-const Player= function createPlayer() {
-    //player names
+const player = () => {
+    const numX = 0;
+    const numO = 0;
+    const playerTurn = () => {
+        if (numX > numO) {
+            return 'p2'
+        }
+        if (numx === numO) {
+            return 'p1'
+        }
+    }
 
-    //player icons
+    //player turns who starts where
 
-    //player turns
+    //winning combinations
+    const winning = () => {
+        const winCombos = {
+            0: [0, 1, 2],
+            1: [3, 4, 5],
+            2: [6, 7, 8],
+            3: [0, 3, 6],
+            4: [1, 4, 7],
+            5: [2, 5, 8],
+            6: [0, 4, 8],
+            7: [2, 4, 6]
+        }
+        //need to check the board and count X and O locations
 
-    //player win totals
 
-    //winning boxes
 
-}
+        //player win totals
+
+    }
+    return {
+        playerTurn
+    }
+};
